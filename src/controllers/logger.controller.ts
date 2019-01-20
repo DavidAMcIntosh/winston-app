881d10a3848769c2cd487f50e5d01e8d5633ee87
DavidAMcIntosh <davidmcintoshbz95@gmail.com>
2019-01-20 12:11:56 -0600

    add winston logger and custom info logs

--- src/controllers/logger.controller.ts
+++ src/controllers/logger.controller.ts
@@ -1,17 +1,27 @@
-import {Request, RestBindings, get, ResponseObject} from '@loopback/rest';
+import {Request, RestBindings, get} from '@loopback/rest';
 import {inject} from '@loopback/context';
 import * as winston from 'winston';
-import * as winstonDailyRotateFile from 'winston-daily-rotate-file';
 
 /**
  * A simple controller to perform some basic logging
  */
 export class LoggerController {
+  public logger = winston.loggers.get('customLogger');
+
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
 
   //tslint:disable no-any
   @get('/log')
   async log(): Promise<any> {
-    return new Promise((resolve, reject) => {});
+    return new Promise((resolve, reject) => {
+      this.logger.info('Begin Custom Logging!');
+      this.logger.info(
+        'Logging, using winston and winston-daily-rotate-file packages.',
+      );
+      this.logger.info('Resource: https://github.com/winstonjs');
+      this.logger.info('This will be logged to ./logs');
+      this.logger.info('End Custom Logging!');
+      resolve('Awesome! Logs outputted to the console and to ./logs');
+    });
   }
 }
